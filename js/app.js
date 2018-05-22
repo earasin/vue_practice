d1c75212c96f1c201fe0fa0cc4d63764b7f39629
Earvin Asin <08501608@newmont.net>
2018-05-22 13:33:33 -0300

    Uploading to GitHub

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const vm = new Vue({
+	el: '#table-data',
+    data: {
+    	posts: [],
+      search: '',
+      sort: '',
+      options: [
+        { label: 'Ascending', value: 'asc' },
+        { label: 'Descending', value: 'desc' },
+      ]
+    },
+    mounted() {
+      axios.get(url).then(response => {
+        this.posts = response.data
+      })
+    },
+		computed: {
+		filteredPosts() {
+				return this.posts.filter(post => {
+					return post.title.toLowerCase().includes(this.search.toLowerCase())
+				});
+			}
+		}
+
+});
